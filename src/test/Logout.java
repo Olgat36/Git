142928ef2157d76a40db11c9e35c4e1ffab4c1e4
Olgat36 <olgat36@gmail.com>
2020-02-29 12:00:54 -0500

    add ReserveRoom.java

--- src/test/Logout.java
+++ src/test/Logout.java
@@ -2,4 +2,6 @@ package test;
 
 public class Logout {
     //some code
+    //updated new code
+
 }
---
+++ src/test/ReserveRoom.java
@@ -0,0 +1,8 @@
+package test;
+
+public class ReserveRoom {
+    public static void main(String[] args) {
+        //only team lead will be able to reserve a room
+
+    }
+}
